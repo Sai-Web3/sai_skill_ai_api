3ade22ea072ead404bfa7dbb9df7cc131e3f0085
HarunoriKawano <harunori.kawano@gmail.com>
2023-06-18 22:44:04 +0900

    update api

--- skill_detector/openai_apis.py
+++ skill_detector/openai_apis.py
@@ -5,9 +5,9 @@ from sai_skill_api.settings import OPENAI_QUERY_EMBEDDING_ENGINE, OPENAI_DOC_EMB
 
 # 履歴を引数として受け取り, 埋め込みベクトルを返す
 def get_career_vector(input_text: str) -> np.ndarray:
-    additional_prompt = "\nThe above is a summary of the answers to the questions about their employment history. " \
-                "We would like to calculate the skills a person would have based on his/her work history using " \
-                "the cosine similarity, so please output a vector for this purpose."
+    additional_prompt = "\r\n\r\nAbove is a question and the answer about someone's work history. We would like to " \
+                        "calculate the skills a person would have based on the " \
+                        "work history using the cosine similarity, so please output a vector for this purpose."
 
     embedding = get_embedding(input_text + additional_prompt, engine=OPENAI_DOC_EMBEDDING_ENGINE)
     return np.array(embedding)
