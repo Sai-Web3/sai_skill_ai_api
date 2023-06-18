22a89be0b6aff738f282d89270ee467717297691
HarunoriKawano <harunori.kawano@gmail.com>
2023-06-18 17:18:13 +0900

    add test

--- skill_detector/apis.py
+++ skill_detector/apis.py
@@ -1,7 +1,6 @@
 import json
 import datetime
 from typing import Optional
-import time
 
 from rest_framework import views, status, permissions
 from rest_framework.response import Response
@@ -25,10 +24,8 @@ def record_career(
     # 1年を1ポイントとしてスケーリング
     scaling_point = term.days / 365
 
-    start = time.time()
     skill_similarities = get_skill_similarity(skill_vectors, career_vector)
     skill_scores = similarities_to_scores(skill_similarities, scaling_point)
-    print(time.time() - start)
 
     if career_id is None:
         current_career_id = insert_career(
